c10ba69933481971d147da7f9fedb7e605bb046d
BrandenBent <bent_branden@hotmail.com>
2019-07-07 17:44:11 -0600

    Obj2 Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,15 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    name = "Branden Bent";
+    age = 23;
+    state = "Minnesota";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
