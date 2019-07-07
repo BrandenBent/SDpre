96bc5583cd0b5d550e86db4203734b2eced9771d
BrandenBent <bent_branden@hotmail.com>
2019-07-07 17:49:45 -0600

    Obj2 Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,16 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "Sushi";
+    food2 = "Chinese Food";
+    food3 = "Steak";
+
+    System.out.println("My Favorite Foods Are:");
+    System.out.println("1." + food1);
+    System.out.println("2." + food2);
+    System.out.println("3." + food3);
+
+
+  }
+}
