d86890bc88ba3a449b8eea38ec4ba7c6f1852511
BrandenBent <bent_branden@hotmail.com>
2019-07-07 15:46:51 -0600

    finish java the hardway ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main(String[] args) {
+    System.out.println("I am determined that you will learn code");
+    System.out.println("today is a great day");
+    }
+  }
