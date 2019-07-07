1db030f308a29b236ea40eaf2091836ad315723f
BrandenBent <bent_branden@hotmail.com>
2019-07-07 16:51:50 -0600

    obj1 Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3 {
+  public static void main( String [] args) {
+    System.out.println("Name: Branden Bent");
+    System.out.println("Age: 23");
+    System.out.println("State: Minnesota");
+  }
+
+}
