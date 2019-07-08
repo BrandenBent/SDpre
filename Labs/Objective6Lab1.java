a905f02137e59952c212758cba5c8ae5ff1f20b6
BrandenBent <bent_branden@hotmail.com>
2019-07-08 06:06:30 -0600

    Obj6 Lab1

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,17 @@
+public class Objective6Lab1 {
+
+  public static void main( String[] args) {
+    int counter = 10;
+
+    while(counter > -1) {
+      System.out.println(counter);
+      counter = (counter - 1);
+    }
+
+  }
+
+
+
+
+
+}
