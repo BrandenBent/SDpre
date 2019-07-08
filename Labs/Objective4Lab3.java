abd1b4fc10bff3b56d601a24d18c14ff2d14f2ff
BrandenBent <bent_branden@hotmail.com>
2019-07-08 05:22:58 -0600

    Obj4 Lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main( String[] args) {
+    Scanner input = new Scanner(System.in);
+    int userNum;
+
+    System.out.println("Please enter a number: " );
+    userNum = input.nextInt();
+
+    if ( userNum > 0 )
+      {System.out.println(userNum + " \n is positive.");
+      }
+    if ( userNum == 0 )
+      {System.out.println(userNum + " \n is equal to Zero and therefore is neither positive nor negative. ");
+      }
+    if ( userNum < 0 )
+      {System.out.println(userNum + " \nis negative.");
+
+
+
+      }
+
+
+
+
+  }
+
+
+
+
+
+
+
+}
